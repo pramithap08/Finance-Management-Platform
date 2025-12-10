7990fa459db194742fd5b14306bf198f14cba58a
Pramitha P <pramithap880@gmail.com>
2025-12-10 22:31:17 +0530

    Transaction form added

---
+++ app/(main)/transaction/create/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { defaultCategories } from "@/data/categories";
+import { getUserAccounts } from "@/actions/dashboard";
+import AddTransactionForm from '../_components/transaction-form';
+const  AddTranactionPage = async () => {
+    const accounts = await getUserAccounts();
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        // editMode={!!editId}
+        // initialData={initialData}
+      />
+    </div>
+  )
+}
+
+export default AddTranactionPage
