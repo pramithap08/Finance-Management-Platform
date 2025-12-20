f415209b636ce0eb6657df0c224b05944ee2d0cd
Pramitha P <pramithap880@gmail.com>
2025-12-20 14:03:30 +0530

    Middleware issue

--- middleware.js
+++ middleware.js
@@ -3,55 +3,51 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/account(.*)",
-  "/transaction(.*)",
-]);
+    "/dashboard(.*)",
+    "/account(.*)",
+    "/transaction(.*)",
+])
 
-// Create Arcjet middleware
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
-  // characteristics: ["userId"], // Track based on Clerk userId
+  
   rules: [
-    // Shield protection for content and security
+    
     shield({
-      mode: "DRY_RUN", // Changed from LIVE to DRY_RUN
+      mode: "LIVE",
     }),
     detectBot({
-      mode: "DRY_RUN", // Changed from LIVE to DRY_RUN
+      mode: "LIVE", 
       allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        "GO_HTTP", // For Inngest
-        // See the full list at https://arcjet.com/bot-list
+        "CATEGORY:SEARCH_ENGINE", 
+        "GO_HTTP", 
       ],
     }),
   ],
 });
 
-// Create base Clerk middleware
-const clerk = clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
+const clerk = clerkMiddleware(async (auth,req)=>{
+    const {userId} = await auth();
 
-  if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
-    return redirectToSignIn();
-  }
+    if(!userId && isProtectedRoute(req)){
+        const {redirectToSignIn} = await auth();
+        return redirectToSignIn();
+        // return redirectToSignIn({ returnBackUrl: req.url });  This sends the user back to the page they tried to access after login.
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 });
 
-// Chain middlewares - ArcJet runs first, then Clerk
 export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    '/(api|trpc)(.*)',
   ],
 };
-
 // import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 // import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 // import { NextResponse } from "next/server";
